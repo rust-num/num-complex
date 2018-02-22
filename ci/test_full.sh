b4c61eac254a55fabd7f504003d01f7e98ce5e12
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-02-23 05:26:50 +0900

    Update CI script

--- ci/test_full.sh
+++ ci/test_full.sh
@@ -12,12 +12,19 @@ cargo test --verbose
 cargo build --no-default-features
 cargo test --no-default-features
 
-# Each isolated feature should also work everywhere.
-for feature in rustc-serialize serde; do
-  cargo build --verbose --no-default-features --features="$feature"
-  cargo test --verbose --no-default-features --features="$feature"
+# It should build with std
+cargo build --no-default-features --features="std"
+cargo test --no-default-features --features="std"
+
+# It should build with both serialization library
+for serialize in rustc-serialize serde; do
+  cargo build --verbose --no-default-features --features="$serialize"
+  cargo test --verbose --no-default-features --features="$serialize"
+  cargo build --verbose --no-default-features --features="std,$serialize"
+  cargo test --verbose --no-default-features --features="std,$serialize"
 done
 
 # Downgrade serde and build test the 0.7.0 channel as well
 cargo update -p serde --precise 0.7.0
 cargo build --verbose --no-default-features --features "serde"
+cargo build --verbose --no-default-features --features "std,serde"
