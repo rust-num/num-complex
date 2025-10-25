1a0ab93fe51192f5099c7b5e183a0918ade420f5
Paul <paulxicao7@gmail.com>
2025-10-25 14:24:46 +0200

    move trait bounds into where clause

--- src/lib.rs
+++ src/lib.rs
@@ -831,8 +831,9 @@ pub trait DivAdd<Rhs = Self, Addend = Self> {
 
 // (a + i b) / (c + i d) + (e + i f) == [(a + i b) * (c - i d)] / (c*c + d*d) + (e + i f)
 //   == {(a*c + b*d) + i (-a*d + b*c)} / n + (e + i f)   for  n=(c*c + d*d)
-impl<T: Clone + Num + Mul<Output = T> + MulAdd<Output = T> + Neg<Output = T>> DivAdd<Complex<T>>
-    for Complex<T>
+impl<T> DivAdd<Complex<T>> for Complex<T>
+where
+    T: Clone + Num + Mul<Output = T> + MulAdd<Output = T> + Neg<Output = T>,
 {
     type Output = Self;
 
@@ -848,7 +849,10 @@ impl<T: Clone + Num + Mul<Output = T> + MulAdd<Output = T> + Neg<Output = T>> Di
         Self::new(re, im) / n + add
     }
 }
-impl<T: Clone + Num + MulAdd<Output = T> + Neg<Output = T>> DivAdd<&Complex<T>> for &Complex<T> {
+impl<T> DivAdd<&Complex<T>> for &Complex<T>
+where
+    T: Clone + Num + Mul<Output = T> + MulAdd<Output = T> + Neg<Output = T>,
+{
     type Output = Complex<T>;
 
     #[inline]
